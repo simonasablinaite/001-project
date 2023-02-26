00f9c38138da2f0de14a6fd7da81effacc49f8de
Simona <simonasablinaite@gmail.com>
2023-02-26 10:14:38 +0200

    Shrink down update

--- script.js
+++ script.js
@@ -33,18 +33,19 @@ class Particle {
    constructor() {
       this.x = Math.random() * canvas.width;
       this.y = Math.random() * canvas.height;
-      this.size = Math.random() * 5 + 1;
+      this.size = Math.random() * 15 + 1;
       this.speedX = Math.random() * 3 - 1.5;
       this.speedY = Math.random() * 3 - 1.5;
    }
    update() {
       this.x += this.speedX;
       this.y += this.speedY;
+      if (this.size > 0.2) this.size -= 0.1;
    }
    draw() {
       ctx.fillStyle = 'yellowgreen';
       ctx.beginPath();
-      ctx.arc(this.x, this.y, 20, 0, Math.PI * 2);
+      ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
       ctx.fill(); //uzpildo apskritima spalva
    }
 }
