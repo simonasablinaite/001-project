c6e0e0bcdb099bd61ce4ecb172da10f729687cc8
Simona <simonasablinaite@gmail.com>
2023-02-26 10:29:49 +0200

    "Oxygen" bubbles style

--- script.js
+++ script.js
@@ -20,19 +20,27 @@ const mouse = {
 canvas.addEventListener('click', function (event) {
    mouse.x = event.x;
    mouse.y = event.y;
-   drawCircle();
+   for (let i = 0; i < 10; i++) {
+      particlesArray.push(new Particle());
+   }
+
 });
 
 canvas.addEventListener('mousemove', function (event) {
    mouse.x = event.x;
    mouse.y = event.y;
-   drawCircle();
+   for (let i = 0; i < 10; i++) {
+      particlesArray.push(new Particle());
+   }
+
 })
 
 class Particle {
    constructor() {
-      this.x = Math.random() * canvas.width;
-      this.y = Math.random() * canvas.height;
+      this.x = mouse.x;
+      this.y = mouse.y;
+      // this.x = Math.random() * canvas.width;
+      // this.y = Math.random() * canvas.height;
       this.size = Math.random() * 15 + 1;
       this.speedX = Math.random() * 3 - 1.5;
       this.speedY = Math.random() * 3 - 1.5;
@@ -43,27 +51,23 @@ class Particle {
       if (this.size > 0.2) this.size -= 0.1;
    }
    draw() {
-      ctx.fillStyle = 'yellowgreen';
+      ctx.fillStyle = 'white';
       ctx.beginPath();
       ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
       ctx.fill(); //uzpildo apskritima spalva
    }
 }
 
-function init() {
-   for (let i = 0; i < 100; i++) {
-      particlesArray.push(new Particle());
-   }
-}
-init();
-
 function handleParticles() {
    for (let i = 0; i < particlesArray.length; i++) {
       particlesArray[i].update();
       particlesArray[i].draw();
+      if (particlesArray[i].size <= 0.3) {
+         particlesArray.splice(i, 1);
+         i--;
+      }
    }
 }
-
 
 function animate() {
    ctx.clearRect(0, 0, canvas.width, canvas.height);
